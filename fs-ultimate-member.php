4d2848b734d2d8d95be9ffdc8c14b86ef1741809
Vitaliy Karakushan <karakushan@gmail.com>
2017-11-26 23:56:30 +0200

    вывел способы онлайн оплаты в деталях заказа

--- fs-ultimate-member.php
+++ fs-ultimate-member.php
@@ -44,6 +44,7 @@ function um_account_content_hook_mytab( $output ) {
 	ob_start();
 
 	$user_orders = \FS\FS_Orders_Class::get_user_orders();
+	$payment     = new FS\FS_Payment_Class();
 	?>
   <div class="um-account-heading uimob340-hide uimob500-hide"><i class="um-faicon-shopping-cart"></i>Мои заказы</div>
 	<?php if ( ! empty( $_GET['order_detail'] ) ): ?>
@@ -54,24 +55,45 @@ function um_account_content_hook_mytab( $output ) {
     <style>
       .order-detail {
         margin: 21px 0;
-        border: 1px solid #eee;
+        border: 1px solid #dddddd;
         padding: 10px;
+        background: #eee;
       }
+
       .order-detail-title {
-        margin: 10px 0;
+        margin: 3px 0 14px 0;
         font-weight: bold;
+        text-transform: uppercase;
       }
+
       .order-detail .thumb img {
         height: auto;
-        width: 138px;
+        width: 80px;
+      }
+
+      .order-detail .table thead td {
+        background: #fff;
+      }
+
+      .order-detail .table thead td:nth-child(3) {
+        width: 116px;
+      }
+
+      .order-detail .table {
+        background: #fff;
+        font-size: 14px;
+      }
+
+      .order-detail tfoot {
+        font-weight: bold;
       }
     </style>
     <div class="order-detail">
       <div class="order-detail-title">Детали заказа #<?php echo $order_id ?></div>
-      <table class="table table-striped">
+      <table class="table">
         <thead>
         <tr>
-          <td>#</td>
+          <td>#ID</td>
           <td>Фото</td>
           <td>Название</td>
           <td>Цена</td>
@@ -86,14 +108,25 @@ function um_account_content_hook_mytab( $output ) {
               <td><?php echo $id ?></td>
               <td class="thumb"><?php if ( has_post_thumbnail( $id ) )
 					  echo get_the_post_thumbnail( $id ) ?></td>
-              <td><?php echo get_the_title( $id ) ?></td>
+              <td><a href="<?php the_permalink( $id ) ?>" target="_blank"><?php echo get_the_title( $id ) ?></a></td>
               <td><?php do_action( 'fs_the_price', $id ) ?></td>
               <td><?php echo $item['count'] ?></td>
-              <td><?php echo $order->sum ?> <?php echo fs_currency() ?></td>
+              <td><?php echo fs_row_price( $id, $item['count'] ) ?></td>
             </tr>
 			<?php endforeach; ?>
 		<?php endif; ?>
+        <tfoot>
+        <tr>
+          <td colspan="5">Общая стоимость</td>
+          <td><?php echo $order->sum ?><?php echo fs_currency() ?></td>
+        </tr>
+        <tr>
+          <td colspan="6">Оплатить онлайн
+			  <?php $payment->show_payment_methods( $order_id ); ?>
+          </td>
+        </tr>
 
+        </tfoot>
         </tbody>
       </table>
     </div>
@@ -119,10 +152,11 @@ function um_account_content_hook_mytab( $output ) {
         <tr>
           <td><?php the_ID(); ?></td>
           <td><?php the_time( 'd.m.Y' ) ?></td>
-          <td><?php echo $order->sum ?><?php echo fs_currency() ?></td>
+          <td><?php echo $order->sum ?> <?php echo fs_currency() ?></td>
           <td><?php echo $order->status ?></td>
-          <td><a href="<?php echo esc_url( add_query_arg( array( 'order_detail' => $post->ID ) ) ) ?>"
-                 class="fs-order-detail">Детали</a></td>
+          <td><a
+              href="<?php echo esc_url( add_query_arg( array( 'order_detail' => $post->ID ), '/account/orders/' ) ) ?>"
+              class="fs-order-detail">Детали</a></td>
         </tr>
 	  <?php endwhile;
 		  wp_reset_query(); ?>
